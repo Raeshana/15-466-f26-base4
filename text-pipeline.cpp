ac56380072c6106037675baca2abae0bcc0aadd7
Raeshana <rsookhoo@andrew.cmu.edu>
2026-09-21 21:20:23 -0400

    text-pipeline.cpp compiles and outputs glyph positions

--- text-pipeline.cpp
+++ text-pipeline.cpp
@@ -6,6 +6,9 @@
 
 #include <iostream>
 
+#define FONT_SIZE 36
+#define MARGGIN (FONT_SIZE * 0.5)
+
 // Referenced https://github.com/harfbuzz/harfbuzz-tutorial/blob/master/hello-harfbuzz-freetype.c
 // Referenced https://freetype.org/freetype2/docs/tutorial/step1.html
 
@@ -13,8 +16,12 @@ int main(int argc, char **argv) {
     const char *fontfile; // 1st argument is the fontfile
     const char *text; // 2nd argument is the text
 
+    fontfile = argv[1];
+    text = argv[2];
+
     if (argc < 3) {
         fprintf (stderr, "usage: hello-harfbuzz font-file.ttf text\n");
+        return 1;
     }
 
     // Initialize library and load font face
@@ -31,10 +38,14 @@ int main(int argc, char **argv) {
         fprintf (stderr, "Font file could be opened and read, but font format is unsupported\n");
         abort();
     }
-    if (ft_error = FT_Err_Unknown_File_Format ) {
-        fprintf (stderr, "Font file could be not be opened or read, or is broken\n");
+    if ((ft_error = FT_Set_Char_Size (ft_face, FONT_SIZE*64, FONT_SIZE*64, 0, 0))){   
+        fprintf (stderr, "Font size could not be set\n");
         abort();
     }
+    // if (ft_error = FT_Err_Unknown_File_Format ) {
+    //     fprintf (stderr, "Font file could be not be opened or read, or is broken\n");
+    //     abort();
+    // }
 
     // Create hb-ft font
     hb_font_t *hb_font = hb_ft_font_create(ft_face, NULL);
@@ -48,9 +59,48 @@ int main(int argc, char **argv) {
     hb_shape(hb_font, hb_buffer, NULL, 0);
 
     // Get glyph info and pos out of the buffer
-    unsigned int len = hb_buffer_get_length (hb_buffer);
+    unsigned int num_chars = hb_buffer_get_length (hb_buffer);
     hb_glyph_info_t *info = hb_buffer_get_glyph_infos (hb_buffer, NULL);
     hb_glyph_position_t *pos = hb_buffer_get_glyph_positions (hb_buffer, NULL);
 
+    // Looks up the glyph index corresponding to the given charcode in the charmap that is currently selected for the face
+    FT_GlyphSlot slot = ft_face->glyph;
+
+    // Pen position
+    int pen_x, pen_y, n;
+    pen_x = 300;
+    pen_y = 200;
+
+    for (n = 0; n < num_chars; n++) {
+        FT_UInt glyph_index;
+
+        // Retrieve glyph id (from harfbuzz info)
+        glyph_index = info[n].codepoint;
+        
+        // Load glyph image into the slot (erase prev)
+        if (ft_error = FT_Load_Glyph (ft_face, glyph_index, FT_LOAD_DEFAULT)) {
+            fprintf (stderr, "Error loading glyph image into slot\n");
+            abort();
+        }
+
+        // Convert to an anti-aliased bitmap
+        if (ft_error = FT_Render_Glyph( ft_face->glyph, FT_RENDER_MODE_NORMAL)) {
+            fprintf (stderr, "Error converting glyph to an anti-aliased bitmap\n");
+            abort();
+        }
+
+        std::cout << "width: " << ft_face->glyph->bitmap.width;
+        std::cout << "height: " << ft_face->glyph->bitmap.rows;
+
+        // // Now, draw to our target surface
+        // my_draw_bitmap( &slot->bitmap,
+        //                 pen_x + slot->bitmap_left,
+        //                 pen_y - slot->bitmap_top );
+
+        // Increment pen position (based on harfbuzz pos)
+        pen_x += pos[n].x_advance;
+        pen_y += pos[n].y_advance;
+    }
+    
 	std::cout << "It worked?" << std::endl;
 }
